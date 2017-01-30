b03cd964714f8b96905887da46004867aa2e4b08
Shadaj Laddad <shadaj@users.noreply.github.com>
2017-01-29 18:43:17 -0800

    Reduce memory usage of dashboard

--- jvm/src/main/scala/com/lynbrookrobotics/funkydashboard/FunkyDashboard.scala
+++ jvm/src/main/scala/com/lynbrookrobotics/funkydashboard/FunkyDashboard.scala
@@ -4,6 +4,7 @@ import akka.stream.scaladsl.{Sink, Source}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.ws.{TextMessage, UpgradeToWebSocket}
 import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
 
 import scala.collection.mutable
 import scala.concurrent.duration._
@@ -12,41 +13,45 @@ import upickle.default._
 import scala.language.postfixOps
 
 class FunkyDashboard {
-  private val datasetGroups = mutable.Map[String, DatasetGroup]()
+  private var datasetGroups = Map[String, DatasetGroup]()
 
-  val startTime = System.currentTimeMillis()
-
-  val source = Source.tick(0 millis, 125 millis, ()).map { _ =>
+  private val source = Source.tick(0 millis, 125 millis, ()).map { _ =>
     val time = System.currentTimeMillis()
-    val toSend = (time, datasetGroups.toMap.map(t => t._1 -> t._2.currentValue))
+    val toSend = (time, datasetGroups.map(t => t._1 -> t._2.currentValue))
     TextMessage(write(toSend))
   }
 
-  val route = get {
+  private val sink = Sink.ignore
+
+  val route: Route = get {
     pathSingleSlash {
       getFromResource("META-INF/resources/index.html")
-    } ~ pathPrefix("") {
-      encodeResponse(getFromResourceDirectory("META-INF/resources"))
-    } ~
-    path("datasets.json") {
+    } ~ path("datasets.json") {
       complete(HttpResponse(
         entity = HttpEntity(
           ContentTypes.`application/json`,
-          write(datasetGroups.values.toVector.map(_.properties))
+          write(datasetGroups.values.map(_.properties))
         )
       ))
     } ~ path("datastream") {
       optionalHeaderValueByType[UpgradeToWebSocket](()) {
         case Some(header) =>
-          complete(header.handleMessagesWithSinkSource(Sink.ignore, source))
+          complete(header.handleMessagesWithSinkSource(sink, source))
         case None =>
           complete(HttpResponse(
             status = StatusCodes.BadRequest,
             entity = "Expected websocket request"
           ))
       }
+    } ~ pathPrefix("") {
+      encodeResponse(getFromResourceDirectory("META-INF/resources"))
     }
   }
 
-  def datasetGroup(key: String) = datasetGroups.getOrElseUpdate(key, new DatasetGroup(key))
+  def datasetGroup(key: String) =
+    datasetGroups.getOrElse(key, {
+      val group = new DatasetGroup(key)
+      datasetGroups = datasetGroups + (key -> group)
+      group
+    })
 }
